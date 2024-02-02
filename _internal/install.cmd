3ff00bb500dd2f38f7955c17197048630a0ba566
Crewsty <116113518+Crewsty@users.noreply.github.com>
2024-02-02 23:43:19 +0100

    Add files via upload

---
+++ _internal/install.cmd
@@ -0,0 +1,15 @@
+pip install requests==2.28.2
+pip install pyfiglet==0.8.post1 
+pip install colorama==0.4.6
+pip install random2==1.0.1
+pip install string==1.6.2
+pip install datetime==4.7
+pip install jsonlib==1.6.1  
+pip install binascii==0.1.0
+pip install uuid==1.30
+pip install subprocess==1.0.0
+pip install hmac==1.1.2
+pip install hashlib==3.9.0
+pip install pywin32==301
+pip install ctypes
+
